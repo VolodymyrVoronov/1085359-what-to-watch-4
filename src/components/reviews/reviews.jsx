1c0f0e89f136bd52ddbd7b9067170741b9eea3cc
Volodymyr <malones.vladimir@gmail.com>
2020-07-07 19:08:17 +0200

    adds some changes in reviews.jsx

--- src/components/reviews/reviews.jsx
+++ src/components/reviews/reviews.jsx
@@ -15,55 +15,39 @@ class Reviews extends PureComponent {
 
     const reviewsOne = reviews.slice(reviews.length / DIVIDER);
     const reviewsTwo = reviews.slice(0, reviews.length / DIVIDER);
+    const reviewsInColumns = [reviewsOne, reviewsTwo];
 
     return (
       <div className="movie-card__reviews movie-card__row">
-        <div className="movie-card__reviews-col">
-          {
-            reviewsOne.map((review, index) => {
+        {
+          reviewsInColumns.map((reviewsInColumns, index) => {
+            return (
+              <div className="movie-card__reviews-col" key={index + Math.random() + Math.random()}>
+                {
+                  reviewsInColumns.map((review, index) => {
 
-              const date = new Date(review.date);
+                    const date = new Date(review.date);
 
-              return (
-                <div key={index} className="review">
-                  <blockquote className="review__quote">
-                    <p className="review__text">{review.text}</p>
-                    <footer className="review__details">
-                      <cite className="review__author">{review.author}</cite>
-                      <time className="review__date" dateTime={date.toISOString()}>{
-                        date.toLocaleDateString(`en-US`, {month: `long`, day: `numeric`, year: `numeric`})
-                      }</time>
-                    </footer>
-                  </blockquote>
-                  <div className="review__rating">{review.score}</div>
-                </div>
-              );
-            })
-          }
-        </div>
-        <div className="movie-card__reviews-col">
-          {
-            reviewsTwo.map((review, index) => {
-
-              const date = new Date(review.date);
-
-              return (
-                <div key={index} className="review">
-                  <blockquote className="review__quote">
-                    <p className="review__text">{review.text}</p>
-                    <footer className="review__details">
-                      <cite className="review__author">{review.author}</cite>
-                      <time className="review__date" dateTime={date.toISOString()}>{
-                        date.toLocaleDateString(`en-US`, {month: `long`, day: `numeric`, year: `numeric`})
-                      }</time>
-                    </footer>
-                  </blockquote>
-                  <div className="review__rating">{review.score}</div>
-                </div>
-              );
-            })
-          }
-        </div>
+                    return (
+                      <div key={index} className="review">
+                        <blockquote className="review__quote">
+                          <p className="review__text">{review.text}</p>
+                          <footer className="review__details">
+                            <cite className="review__author">{review.author}</cite>
+                            <time className="review__date" dateTime={date.toISOString()}>{
+                              date.toLocaleDateString(`en-US`, {month: `long`, day: `numeric`, year: `numeric`})
+                            }</time>
+                          </footer>
+                        </blockquote>
+                        <div className="review__rating">{review.score}</div>
+                      </div>
+                    );
+                  })
+                }
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
