691af42b1f556606e2c8fa0c84930ebba6d2124d
Volodymyr <malones.vladimir@gmail.com>
2020-08-05 09:02:02 +0200

    updated code in reviews.js

--- src/components/reviews/reviews.test.js
+++ src/components/reviews/reviews.test.js
@@ -23,46 +23,6 @@ const REVIEWS = [
     date: `November 18, 2015`,
     rating: 8.0,
   },
-  {
-    id: 3,
-    user: {
-      id: 3,
-      name: `Amanda Greever`,
-    },
-    comment: `I didn't find it amusing, and while I can appreciate the creativity, it's an hour and 40 minutes I wish I could take back.`,
-    date: `November 18, 2015`,
-    rating: 8.0,
-  },
-  {
-    id: 4,
-    user: {
-      id: 4,
-      name: `Matthew Lickona`,
-    },
-    comment: `The mannered, madcap proceedings are often delightful, occasionally silly, and here and there, gruesome and/or heartbreaking.`,
-    date: `December 20, 2016`,
-    rating: 7.2,
-  },
-  {
-    id: 5,
-    user: {
-      id: 5,
-      name: `Paula Fleri-Soler`,
-    },
-    comment: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
-    date: `December 20, 2016`,
-    rating: 7.6,
-  },
-  {
-    id: 6,
-    user: {
-      id: 6,
-      name: `Paula Fleri-Soler`,
-    },
-    comment: `It is certainly a magical and childlike way of storytelling, even if the content is a little more adult.`,
-    date: `December 20, 2016`,
-    rating: 7.0,
-  },
 ];
 
 it(`render should be match markup`, () => {
