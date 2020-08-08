cc5f5354b76ab4183b84616e49b690dbacce2d75
Volodymyr <malones.vladimir@gmail.com>
2020-08-08 23:42:21 +0200

    fixed errors after first review

--- src/components/const.js
+++ src/components/const.js
@@ -1,24 +1,18 @@
 export const ALL_GENRE = `All genres`;
-const COMEDY_GENRE = `Comedy`;
-const CRIME_GENRE = `Crime`;
-const DOCUMENTARY_GENRE = `Documentary`;
-const DRAMA_GENRE = `Drama`;
-const ROMANCE_GENRE = `Romance`;
-const THRILLER_GENRE = `Thriller`;
 
 export const GENRES = [
-  COMEDY_GENRE,
-  CRIME_GENRE,
-  DOCUMENTARY_GENRE,
-  DRAMA_GENRE,
-  ROMANCE_GENRE,
-  THRILLER_GENRE,
+  `Comedy`,
+  `Crime`,
+  `Documentary`,
+  `Drama`,
+  `Romance`,
+  `Thriller`,
 ];
 
 export const GENRE_ALIASES = {
-  [COMEDY_GENRE]: `Comedies`,
-  [DRAMA_GENRE]: `Dramas`,
-  [THRILLER_GENRE]: `Thrillers`,
+  COMEDY_GENRE: `Comedies`,
+  DRAMA_GENRE: `Dramas`,
+  THRILLER_GENRE: `Thrillers`,
 };
 
 export const SERVER_URL = `https://4.react.pages.academy/wtw`;
